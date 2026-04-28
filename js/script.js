b34240c9f995ef010b86edb82215b2eb43fd2646
Nadazh92 <nadalizaher92@gmail.com>
2026-04-28 12:30:47 +0200

    javascript1-scenario

--- js/script.js
+++ js/script.js
@@ -1,9 +1,56 @@
-const bars = document.querySelector("#bars");
+// ------------navigationen-------------
+// const bars = document.querySelector("#bars");
 
-const openNav = () => {
-    document.querySelector("header > nav > ul").classList.toggle("show");
-    document.querySelector("#bars i").classList.toggle("fa-bars");
-    document.querySelector("#bars i").classList.toggle("fa-xmark");
+// const openNav = () => {
+//     document.querySelector("header > nav > ul").classList.toggle("show");
+//     document.querySelector("#bars i").classList.toggle("fa-bars");
+//     document.querySelector("#bars i").classList.toggle("fa-xmark");
+// }
+
+// bars.addEventListener("click", openNav);
+
+
+// -------------------scenario---------------
+
+const allBtns=document.querySelectorAll(".stage .btn");
+const allStages=document.querySelectorAll(".stage");
+
+function scenario(e){
+    console.log(e.target.textContent);
+    const btnContent=e.target.textContent;
+
+    if(btnContent==="Brug åbent WiFi uden VPN"){
+        allStages[0].classList.remove("active");
+        allStages[1].classList.add("active");
+
+    }else if (btnContent==="Log ind uden at tænke"){
+            allStages[1].classList.remove("active");
+            allStages[2].classList.add("active");
+
+    } else if (btnContent==="Brug VPN"){
+                allStages[1].classList.remove("active");
+                allStages[3].classList.add("active");
+
+    } else if (btnContent==="Tjek Netværk"){
+                allStages[0].classList.remove("active");
+                allStages[4].classList.add("active");
+
+    } else if (btnContent==="Brug Hotspot"){
+                 allStages[0].classList.remove("active");
+                 allStages[5].classList.add("active");
+
+    } else if (btnContent==="Start Forfra") {
+                    allStages[2].classList.remove("active");
+                    allStages[3].classList.remove("active");
+                    allStages[5].classList.remove("active");
+                    allStages[0].classList.add("active");
+               
+    } else {
+        console.log("Error");
+
+    }
 }
 
-bars.addEventListener("click", openNav);
+for (btn of allBtns){
+    btn.addEventListener("click", scenario);
+}
